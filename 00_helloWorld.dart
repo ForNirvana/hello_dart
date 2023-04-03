debba14255fdb8726e4bc904b5e37fcc8abab6f5
ForNirvana qh <forgamorwork@163.com>
2023-04-03 03:14:56 +0000

    language_function

--- 00_helloWorld.dart
+++ 00_helloWorld.dart
@@ -1,23 +1,13 @@
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 void main() {
-  var year = 1999;
-  var flybyobjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var result = fibonacci(20);
+  print(result);
 
-  if (year >= 2001) {
-    print('21st century');
-  } else if (year >= 1901) {
-    print('20th century');
-  }
-
-  for (final object in flybyobjects) {
-    print(object);
-  }
-
-  for (int month = 1; month <= 12; month++) {
-    print(month);
-  }
-
-  while (year < 2023) {
-    year += 1;
-    print(year);
-  }
+  var name = 'Voyager I';
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  flybyObjects.where((name) => name.contains('turn')).forEach(print);
 }
