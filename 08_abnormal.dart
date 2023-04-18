e07778446290824558ee6a63c7597ba8acd669f6
ForNirvana qh <forgamorwork@163.com>
2023-04-18 13:38:29 +0000

    language_abnormal

---
+++ 08_abnormal.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+Future<void> describeFlybyObjects(List<String> flybyObjects) async {
+  try {
+    for (final object in flybyObjects) {
+      var description = await File('$object.txt').readAsString();
+      print(description);
+    }
+  } on IOException catch (e) {
+    print('Could not describe object: $e');
+  } finally {
+    flybyObjects.clear();
+  }
+}
+
+void main() {
+  var astronauts = 1;
+
+  if (astronauts == 0) {
+    throw StateError('No astronauts.');
+  }
+}
