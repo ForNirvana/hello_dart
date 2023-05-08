11e9e77252b8c73ab2b311c27bd4d09fedc711ec
ForNirvana qh <forgamorwork@163.com>
2023-05-08 03:56:07 +0000

    start

---
+++ core_library_dart/03_collections.dart
@@ -0,0 +1,7 @@
+void main() {
+  //As the language tour shows, you can use literals to create and initialize lists.
+  // Alternatively, use one of the List constructors. The List class also defines
+  // several methods for adding items to and removing items from lists.
+
+  print('OK.');
+}
